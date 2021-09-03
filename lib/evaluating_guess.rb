dcb391ee5c306d01c92ba933a0e5470afcd60330
Devin Pile <devinpile@gmail.com>
2021-09-03 01:18:47 -0600

    Add evaluating guess files

---
+++ lib/evaluating_guess.rb
@@ -0,0 +1,27 @@
+class EvaluatingGuess
+
+  def prompt
+    puts "What is your guess?"
+  end
+
+  def guess_attempt
+    gets.chomp
+  end
+
+  def guess(user_guess, secret_code)
+    hint = {correct_colors: 0, correct_positions: 0}
+    if user_guess == secret_code
+      "you win"
+    else
+      user_guess.each_index do |x|
+        secret_code.each_index do |y|
+          if x == y
+            hint[:correct_positions] += 1
+          end
+        end
+      end
+      hash
+    end
+
+  end
+end
