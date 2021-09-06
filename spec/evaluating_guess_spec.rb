dffc9abe2a95beccef26e4597bf4637be79e4a35
Devin Pile <devinpile@gmail.com>
2021-09-06 14:00:12 -0600

    Debug guess method and add game files

---
+++ spec/evaluating_guess_spec.rb
@@ -0,0 +1,46 @@
+require 'rspec'
+require './lib/secret_code'
+require './lib/evaluating_guess'
+
+RSpec.describe EvaluatingGuess do
+  xit 'exist' do
+    attempt = EvaluatingGuess.new
+    expect(attempt).to be_a(EvaluatingGuess)
+  end
+
+  xit 'it prompts the user' do
+    attempt = EvaluatingGuess.new
+    expect do
+      attempt.prompt
+    end.to output("What is your guess?\n").to_stdout
+  end
+
+  xit 'receives user input' do
+    attempt = EvaluatingGuess.new
+    input = StringIO.new("rbgy\n")
+    expect(attempt.guess_attempt).to eq("rbgy")
+  end
+
+  describe "#guess method" do
+    xit 'returns correct' do
+      attempt = EvaluatingGuess.new
+      user_guess = ["g","g","g","g"]
+      secret_code = ["g","g","g","g"]
+      expect(attempt.guess(user_guess,secret_code)).to eq("you win")
+    end
+
+    it 'returns nothing correct' do
+      attempt = EvaluatingGuess.new
+      user_guess = ["r","r","r","r"]
+      secret_code = ["g","g","g","g"]
+      expect(attempt.guess(user_guess,secret_code)).to eq({correct_colors: 0, correct_positions: 0})
+    end
+
+    it 'returns partial correct' do
+      attempt = EvaluatingGuess.new
+      user_guess = ["g","r","g","r"]
+      secret_code = ["g","g","g","g"]
+      expect(attempt.guess(user_guess,secret_code)).to eq({correct_colors: 2, correct_positions: 2})
+    end
+  end
+end
---
+++ spec/secret_code_spec.rb
@@ -0,0 +1,20 @@
+require 'rspec'
+require './lib/secret_code'
+
+RSpec.describe SecretCode do
+  it "exists" do
+    secret_code = SecretCode.new
+    expect(secret_code).to be_an_instance_of(SecretCode)
+  end
+
+  it "can get random sample of 4 colors" do
+    secret_code = SecretCode.new
+    expect(secret_code.colors).to be_an(Array)
+    expect(secret_code.colors.length).to eq(4)
+  end
+
+  it 'generates random code of 4' do
+    secret_code = SecretCode.new
+    expect(secret_code.generate_code.length).to eq(4)
+  end
+end
