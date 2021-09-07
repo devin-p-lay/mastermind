a432f05a83b08bac2f1ea1957a0624f6e93f969d
Mallory Vining <mallorygallery12@gmail.com>
2021-09-07 12:46:43 -0600

    Add changes to guess file

--- lib/evaluating_guess.rb
+++ lib/evaluating_guess.rb
@@ -1,45 +1,31 @@
 require "./lib/secret_code"
+require "./lib/message"
+require "./lib/player"
 
 class EvaluatingGuess
-  # def initialize(secret_code)
-  #   @secret_code = secret_code
-  # end
-
-  def prompt
-    puts "What is your guess?"
+  attr_reader :colors
+  def initialize
+    # @colors = colors
   end
 
-  def guess_attempt
-    #USER FILE???
-    gets.chomp
-  end
 
-  def guess(user_guess, secret_code)
-<<<<<<< HEAD
-    require "pry"; binding.pry
-    secret_code = SecretCode.colors
-=======
->>>>>>> game
+  def guess
+
     hint = {correct_colors: 0, correct_positions: 0}
-    if user_guess == secret_code
-      "you win"
+    if @user_guess == @secret_code
+      win_message
     else
-<<<<<<< HEAD
-      user_guess.each_with_index do |x, index|
-        if x == secret_code[index]
-=======
-      user_guess.each_with_index do |element, index|
-        if element == secret_code[index]
->>>>>>> game
+      @user_guess.each_with_index do |x, index|
+        if x == @secret_code[index]
             hint[:correct_positions] += 1
         end
       end
 
       ["b","r","g","y"].each do |color|
-        user_color = user_guess.find_all do |element|
+        user_color = @user_guess.find_all do |element|
           element == color
         end.count
-        secret_color = secret_code.find_all do |element|
+        secret_color = @secret_code.find_all do |element|
           element == color
         end.count
         if secret_color >= user_color
@@ -48,14 +34,9 @@ class EvaluatingGuess
           hint[:correct_colors] += secret_color
         end
       end
-<<<<<<< HEAD
 
       puts "You have #{hint[:correct_colors]} colors correct and #{hint[:correct_positions]} colors in the correct position."
-
-=======
-      puts "With the guess of #{user_guess}: You have #{hint[:correct_colors]} colors correct and #{hint[:correct_positions]} colors in the correct position."
       hint
->>>>>>> game
     end
   end
 end
