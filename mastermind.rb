08f126c2cd23f249e3dcb9a4bf7a182afca8c63f
Devin Pile <devinpile@gmail.com>
2021-09-06 21:09:56 -0600

    Start our mastermind.rb runner

--- mastermind.rb
+++ mastermind.rb
@@ -1,57 +1,12 @@
-<<<<<<< HEAD
-=======
-<<<<<<< HEAD
->>>>>>> f1ea5e30f7226fed70ab3939fff0f60ba4b269b3
-def intro
-  puts "Welcome to MASTERMIND"
-  puts "Would you like to (p)lay, read the (i)nstructioins, or (q)uit?"
-  get.chomp
-<<<<<<< HEAD
-=======
-=======
-require '.lib/start_game.rb'
-StartGame 
->>>>>>> ddb479ff488706c279dd1d62fce8261958458cc2
->>>>>>> f1ea5e30f7226fed70ab3939fff0f60ba4b269b3
+#this will be our runner file
+# require './lib/secret_code.rb'
+# require './lib/evaluating_guess.rb'
+require './lib/player.rb'
+require './lib/message.rb'
+require './lib/game.rb'
 
+#create menu class???
+player = Player.new
+message = Message.new
 
-
-
-
-
-
-
-<<<<<<< HEAD
-# name = gets.chomp()
-# puts ("Hello " + name + "...are you ready to code??")
-=======
-<<<<<<< HEAD
-# name = gets.chomp()
-# puts ("Hello " + name + "...are you ready to code??")
-=======
-
-
-
-
-
-
-
-# puts "Welcome to MASTERMIND"
-# puts "Would you like to (p)lay the game, read the (i)nstructions, or (q)uit?"
-# starting = gets.chomp
-#
-# if starting == 'p'
-#   puts "play"
-#   #game_flow
-# elsif starting == 'i'
-#   puts "instructions"
-#   instructions
-# elsif starting == 'q'
-#   puts "quit"
-#   #quits program
-# else
-#   puts "try again"
-#   #repeats intro
-# end
->>>>>>> ddb479ff488706c279dd1d62fce8261958458cc2
->>>>>>> f1ea5e30f7226fed70ab3939fff0f60ba4b269b3
+game = Game.new(message, player)
