2691b5f49c09a26e11423e8128dbc85cbd3e4ea0
Devin Pile <devinpile@gmail.com>
2021-09-07 19:00:04 -0600

    Add refactoring to evaluating guess

--- lib/evaluating_guess.rb
+++ lib/evaluating_guess.rb
@@ -4,44 +4,82 @@ require "./lib/player"
 require "./lib/game"
 
 class EvaluatingGuess
-  attr_reader :colors
-  def initialize(player, secret_code)
-    @user_guess = player.player_guess_attempt
+  attr_reader :colors,
+              :player_guess_attempt
+              
+  def initialize(game, secret_code)
+    @user_guess = game.player_guess_attempt
+    # @user_guess = player.player_guess_attempt
     @secret_code = secret_code.colors
   end
 
 
-  def guess
-    hint = {correct_colors: 0, correct_positions: 0}
+
+  def correct_guess
     if @user_guess == @secret_code
       "you win"
-    elsif @user_guess == ["q"]
-      exit
-    elsif @user_guess == ["c"]
-      p @secret_code
-    else
-      @user_guess.each_with_index do |element, index|
-        if element == @secret_code[index]
-            hint[:correct_positions] += 1
-        end
-      end
-
-      ["b","r","g","y"].each do |color|
-        user_color = @user_guess.find_all do |element|
-          element == color
-        end.count
-        secret_color = @secret_code.find_all do |element|
-          element == color
-        end.count
-        if secret_color >= user_color
-          hint[:correct_colors] += user_color
-        else
-          hint[:correct_colors] += secret_color
-        end
-      end
-
-      puts "You have #{hint[:correct_colors]} colors correct and #{hint[:correct_positions]} colors in the correct position."
-
     end
   end
+
+  def guess_color
+    hint = {correct_colors: 0, correct_positions: 0}
+    ["b","r","g","y"].each do |color|
+      user_color = @user_guess.find_all do |element|
+        element == color
+      end.count
+      secret_color = @secret_code.find_all do |element|
+        element == color
+      end.count
+      if secret_color >= user_color
+        hint[:correct_colors] += user_color
+      else
+        hint[:correct_colors] += secret_color
+      end
+    end
+    puts "You have #{hint[:correct_colors]} colors correct"
+  end
+
+  def guess_position
+    hint = {correct_colors: 0, correct_positions: 0}
+    @user_guess.each_with_index do |element, index|
+      if element == @secret_code[index]
+          hint[:correct_positions] += 1
+      end
+    end
+    puts " and #{hint[:correct_positions]} colors in the correct position."
+  end
 end
+
+
+
+
+
+# def guess
+#   hint = {correct_colors: 0, correct_positions: 0}
+#   if @user_guess == @secret_code
+#     "you win"
+#   else
+#     @user_guess.each_with_index do |element, index|
+#       if element == @secret_code[index]
+#           hint[:correct_positions] += 1
+#       end
+#     end
+#
+#     ["b","r","g","y"].each do |color|
+#       user_color = @user_guess.find_all do |element|
+#         element == color
+#       end.count
+#       secret_color = @secret_code.find_all do |element|
+#         element == color
+#       end.count
+#       if secret_color >= user_color
+#         hint[:correct_colors] += user_color
+#       else
+#         hint[:correct_colors] += secret_color
+#       end
+#     end
+#
+#     puts "You have #{hint[:correct_colors]} colors correct and #{hint[:correct_positions]} colors in the correct position."
+#
+#   end
+# end
